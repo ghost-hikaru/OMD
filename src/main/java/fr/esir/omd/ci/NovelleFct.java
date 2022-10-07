7c4f52e6d940a82b0c013c3d8371d0c7aad1d5a3
ghost-hikaru <melvin.mathurin35@gmail.com>
2022-10-07 08:58:21 +0200

    finConflit

--- src/main/java/fr/esir/omd/ci/NovelleFct.java
+++ src/main/java/fr/esir/omd/ci/NovelleFct.java
@@ -1,18 +1,11 @@
 package fr.esir.omd.ci;
 
 public class NovelleFct {
-<<<<<<< HEAD
-=======
 
->>>>>>> a9819d8ef1f250041b7e25f128227c36c2545010
   public static void main(String[] args) {
     // TODO Auto-generated method stub
     System.out.println("Bonjour");	
     System.out.println("conflit");
   }
-<<<<<<< HEAD
-=======
 
-
->>>>>>> a9819d8ef1f250041b7e25f128227c36c2545010
 }
