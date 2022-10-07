58e47dd0a029f61b0c6da267353566409c93842e
ghost-hikaru <melvin.mathurin35@gmail.com>
2022-10-07 08:43:17 +0200

    conflit

--- src/main/java/fr/esir/omd/ci/NovelleFct.java
+++ src/main/java/fr/esir/omd/ci/NovelleFct.java
@@ -4,6 +4,7 @@ public class NovelleFct {
   public static void main(String[] args) {
     // TODO Auto-generated method stub
     System.out.println("Bonjour");	
+    System.out.println("conflit");
   }
 
 }
